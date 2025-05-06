5dac900803b393bcef7cb6e540bf56d6146823a7
zablink <zablinkapp@gmail.com>
2025-05-06 15:58:05 +0700

    auth supabase

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import FacebookProvider from 'next-auth/providers/facebook'
-import { SupabaseAdapter } from '@next-auth/supabase-adapter'
+import { SupabaseAdapter } from '@auth/supabase-adapter'
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 import type { AdapterUser } from 'next-auth/adapters'
 import type { JWT } from 'next-auth/jwt'
