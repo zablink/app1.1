6eb4a795647838ae59a976a3b91303d01a4dcca3
zablink <zablinkapp@gmail.com>
2025-05-06 13:15:41 +0700

    supabase.ts & [...nextauth].ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
-// lib/supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
