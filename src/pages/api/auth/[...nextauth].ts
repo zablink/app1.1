4d6acb467c370b2a93311b3c63f9123c609fbbd4
zablink <zablinkapp@gmail.com>
2025-05-06 13:19:53 +0700

    edit import supabase in [ NxAuth]

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
-import { createClient } from "@/lib/supabase";
+import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient();
 /*
