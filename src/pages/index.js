35b55c8e2f49bec552e59e933ee19b3a76be9364
zablink <zablinkapp@gmail.com>
2025-05-06 13:31:44 +0700

    index change connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import supabase from '../lib/supabase';
+import { supabase } from '@/lib/supabase';;
 
 export default function Home() {
   const [provinces, setProvinces] = useState([]);
