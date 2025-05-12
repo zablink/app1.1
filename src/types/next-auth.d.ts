56b24fe43ccdabc89e9c23f0131a4c7f2f908827
zablink <zablinkapp@gmail.com>
2025-05-12 16:21:48 +0700

    edit next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,19 +1,16 @@
-//import NextAuth from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: { 
-      id?: string; // เพิ่ม id สำหรับ session
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
+    user: {
+      id?: string;
       role?: "user" | "store" | "admin";
       membershipType?: "free" | "pro1" | "pro2" | "pro3" | "special";
-    };
+    } & DefaultSession["user"];
   }
 
-  interface User {
-    id?: string; // เพิ่ม id ใน user
+  interface User extends DefaultUser {
+    id?: string;
     role?: "user" | "store" | "admin";
     membershipType?: "free" | "pro1" | "pro2" | "pro3" | "special";
   }
