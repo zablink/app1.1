9607f11fd7f8c7985a22725c8b5833dbe6da5f5a
zablink <zablinkapp@gmail.com>
2025-05-14 11:34:56 +0700

    change FONT to Thai setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // 👈 เพิ่มตรงนี้
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-body)'],
+        header: ['var(--font-header)'],
+      },
+    },
   },
   plugins: [],
 }
