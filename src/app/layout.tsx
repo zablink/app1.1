f690292a4fa8f63c94ad1e1fea860fb80731f7cc
zablink <zablinkapp@gmail.com>
2025-05-18 00:32:24 +0700

    try favicon again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="th">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={`${notoSansThai.variable} ${kanit.variable}`}>
         {children}
       </body>
