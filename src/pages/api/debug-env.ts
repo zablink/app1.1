e26b0c7a3e45ea278df5dc5c286ac6f840b431bf
zablink <zablinkapp@gmail.com>
2025-05-17 15:34:04 +0700

    debug-env

--- src/pages/api/debug-env.ts
+++ src/pages/api/debug-env.ts
@@ -1,5 +1,6 @@
-// pages/api/debug-env.ts
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
     VERCEL_ENV: process.env.VERCEL_ENV,
     NODE_ENV: process.env.NODE_ENV,
