787199843b7800d8c9acdd343f861cdfbf97089b
zablink <zablinkapp@gmail.com>
2025-05-19 15:28:22 +0700

    test google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,5 @@
 // pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
-import { Noto_Sans_Thai } from "next/font/google";
-import "@/styles/globals.css";
-
-const notoSansThai = Noto_Sans_Thai({
-  subsets: ["thai"],
-  weight: ["300", "400", "500", "700"],
-  variable: "--font-body",
-});
 
 
 export default function Document() {
@@ -17,13 +9,14 @@ export default function Document() {
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Thai:wght@100..900&family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Noto+Serif+Thai:wght@100..900&family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;700&family=Kanit:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
+
       </Head>
       <body>
-        <main className={notoSansThai.variable}>
-          <Component {...pageProps} />
-        </main>
+        <Main />
         <NextScript />
       </body>
     </Html>
