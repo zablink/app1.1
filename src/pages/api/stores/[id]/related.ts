22e7abb9d70eaecea2dd3dabd041dc22d9bde99a
zablink <zablinkapp@gmail.com>
2025-05-30 16:58:11 +0700

    edit review.ts, relate.ts

--- src/pages/api/stores/[id]/related.ts
+++ src/pages/api/stores/[id]/related.ts
@@ -1,6 +1,5 @@
 // /pages/api/store/[id]/related.ts
 
-/*
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/lib/supabase";
 
@@ -15,30 +14,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (error) return res.status(500).json({ error: error.message });
 
   res.status(200).json({ relatedStores: data });
-}
-*/
-
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const relatedStores = [
-    {
-      id: 2,
-      name: "ร้านชานมไข่มุก",
-      subdistrict_name: "บางเขน",
-      district_name: "บางเขน",
-      province_name: "กรุงเทพมหานคร",
-      proximity_level: 1,
-    },
-    {
-      id: 3,
-      name: "ร้านอาหารอีสาน",
-      subdistrict_name: "หลักสี่",
-      district_name: "หลักสี่",
-      province_name: "กรุงเทพมหานคร",
-      proximity_level: 3,
-    },
-  ];
-
-  res.status(200).json({ relatedStores });
 }
