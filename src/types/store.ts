bdc1ceb623679c0ec79505bb048da4599aee04f5
zablink <zablinkapp@gmail.com>
2025-05-31 18:18:06 +0700

    adjust store type

--- src/types/store.ts
+++ src/types/store.ts
@@ -4,8 +4,10 @@ export type Store = {
   id: string;
   user_id: string;
   name: string;
-  category: string;
+  category?: string;
+  description?: string;
   package: string;
+  cover_url?: string;
   subdistrict_id: string;
   latitude: number | null;
   longtitude: number | null;
