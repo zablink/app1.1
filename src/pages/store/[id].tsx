77c03c470f719dd70b83fc33cc26438ab204205b
zablink <zablinkapp@gmail.com>
2025-06-08 11:30:24 +0700

    navbar

--- src/pages/store/[id].tsx
+++ src/pages/store/[id].tsx
@@ -126,7 +126,7 @@ export default function StoreDetailPage() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen bg-neutral px-4 py-8 text-gray-800">
+      <div className="pt-16 min-h-screen bg-neutral px-4 py-8 text-gray-800">
         <div className="max-w-5xl mx-auto space-y-8">
 
           {/* Breadcrumb */}
