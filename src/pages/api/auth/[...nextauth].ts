3844e5e75e0b0aa7cbd2793eab12a1fe32c4f084
zablink <zablinkapp@gmail.com>
2025-07-06 17:10:20 +0700

    fix Tiktok Profile error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -64,7 +64,7 @@ export const authOptions: NextAuthOptions = {
           image: profile.data.user.avatar_url,
           role: "user",
           membershipType: "free",
-        };
+        } as User;
       },
     }),
   ],
