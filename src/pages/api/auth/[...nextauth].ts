201f4f280e9f47816e66b3c559a03a654a32d635
zablink <zablinkapp@gmail.com>
2025-07-06 16:50:49 +0700

    add role , membershipType for Tiktok profile

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -56,7 +56,7 @@ export const authOptions: NextAuthOptions = {
       },
       token: "https://open.tiktokapis.com/v2/oauth/token/",
       userinfo: "https://open.tiktokapis.com/v2/user/info/",
-      profile(profile: any, tokens: any): User {
+      profile(profile, tokens) {
         return {
           id: profile.data.user.open_id,
           name: profile.data.user.display_name,
@@ -64,7 +64,7 @@ export const authOptions: NextAuthOptions = {
           image: profile.data.user.avatar_url,
           role: "user",
           membershipType: "free",
-        };
+        } as const;
       },
     }),
   ],
