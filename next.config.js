7177392df7494a2de7dffa353118f1abbd7c05b9
zablink <zablinkapp@gmail.com>
2025-07-07 15:18:25 +0700

    force no cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store, max-age=0",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
