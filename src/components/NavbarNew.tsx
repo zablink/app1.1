1fbe9c89311ba4a94270291b5ff958714d671736
zablink <zablinkapp@gmail.com>
2025-07-07 22:09:09 +0700

    navbar

--- src/components/NavbarNew.tsx
+++ src/components/NavbarNew.tsx
@@ -42,8 +42,6 @@ export default function Navbar() {
           scrolled ? "shadow-lg" : ""
         }`}
       >
-
-      >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
