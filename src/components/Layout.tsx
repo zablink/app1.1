0bb68efa8940acc67ac0934f387a1003f087ef62
zablink <zablinkapp@gmail.com>
2025-07-07 10:56:52 +0700

    Navbar layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,7 @@
 // /src/components/Layout.tsx
+import { useRef, useEffect, useState } from "react";
+import Navbar, { NavbarRef } from "./Navbar";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
