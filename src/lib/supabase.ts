b067cef6d7192000b796c4e07a5b99992aab82e1
zablink <zablinkapp@gmail.com>
2025-07-13 12:37:26 +0700

    fix login flow for diff login method in same email

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,8 +5,8 @@ import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 // สำหรับฝั่ง client เช่นใน React component
 export const createClient = () =>
   createBrowserClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
 // สำหรับฝั่ง server เช่นใน API routes
