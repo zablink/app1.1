c2d672533bf854a437dee57c631500d73a946863
zablink <zablinkapp@gmail.com>
2025-07-13 12:12:53 +0700

    fix login flow for diff login method in same email

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,8 +13,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // สร้าง Supabase client สำหรับ Adapter
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SECRET_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY!
 );
 
 // กำหนด AuthOptions แยกต่างหาก
