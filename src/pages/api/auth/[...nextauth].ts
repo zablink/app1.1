1410b9821778c4df4a56197f1e74709a3baf40a9
zablink <zablinkapp@gmail.com>
2025-07-24 00:37:50 +0700

    hide social auth temp

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -70,7 +70,7 @@ console.log("------------------------------------");
 // และตั้งค่าใน Vercel Dashboard ด้วยชื่อเดียวกัน: SUPABASE_URL, SUPABASE_ANON_KEY
 const SUPABASE_URL = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY; // ใช้ SERVICE_ROLE_KEY
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY; // ปกติ Adapter จะใช้ SERVICE_ROLE_KEY
+//const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY; // ปกติ Adapter จะใช้ SERVICE_ROLE_KEY
 
 
 // >>> ดึงค่าสำหรับ SupabaseAdapter:
