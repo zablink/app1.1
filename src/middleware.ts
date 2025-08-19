5e28680e886d208902668b82d2732c387da2c44b
zablink <zablinkapp@gmail.com>
2025-08-19 22:50:18 +0700

    modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 // /src/middleware.ts
-import getToken from "next-auth/jwt";ห
+import getToken from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
