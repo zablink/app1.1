1518b35f391e8ff304d56d7f3480a75780abe3dd
zablink <zablinkapp@gmail.com>
2025-08-20 10:05:34 +0700

    modify next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,34 +1,37 @@
-import NextAuth, { DefaultSession } from "next-auth";
+// /types/next-auth.d.ts
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role?: "user" | "shop" | "admin";
-      membership_type?: string;
-      avatar_url?: string | null; // 👈 เพิ่ม null
-      isNewUser?: boolean;
-      provider?: string | null ;
+      role: "user" | "shop" | "admin";
+      membership_type: string;
+      avatar_url: string | null;
+      isNewUser: boolean;
+      provider: string | null;
+      username?: string;
+      points?: number;
     } & DefaultSession["user"];
   }
+
+  interface User extends DefaultUser {
+    role?: "user" | "shop" | "admin";
+    membership_type?: string;
+    avatar_url?: string | null;
+    isNewUser?: boolean;
+  }
+}
 
 declare module "next-auth/jwt" {
   interface JWT {
     id: string;
-    role?: "user" | "shop" | "admin";
-    membership_type?: string;
-    avatar_url?: string | null;   // 👈 เพิ่ม null
-    isNewUser?: boolean;
-    provider?: string | null;
-  }
-}
-
-
-
-  interface User extends DefaultUser {
-    role?: string;
-    membership_type?: string;
-    avatar_url?: string | null;   // 👈 เพิ่ม null
-    isNewUser?: boolean;
+    role: "user" | "shop" | "admin";
+    membership_type: string;
+    avatar_url: string | null;
+    isNewUser: boolean;
+    provider: string | null;
+    username?: string;
+    points?: number;
   }
 }
